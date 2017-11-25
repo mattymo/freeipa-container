ee644592f5f3c8bdc83a2a7af040ac9a7056d2e7
Matthew Mosesohn <matthew.mosesohn@gmail.com>
2017-11-25 12:28:06 +0000

    Add kubectl and method to get token from dex

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,17 @@ FROM centos:centos7
 
 MAINTAINER Jan Pazdziora
 
-RUN yum swap -y -- remove fakesystemd -- install systemd systemd-libs && yum clean all
+#Add kubectl
+ADD https://storage.googleapis.com/kubernetes-release/release/v1.8.3/bin/linux/amd64/kubectl /usr/local/bin/kubectl
+RUN chmod +x /usr/local/bin/kubectl
 
-# Install FreeIPA client
-RUN yum install -y ipa-client dbus-python perl 'perl(Data::Dumper)' 'perl(Time::HiRes)' openssh-clients openssh-server openldap-clients && yum clean all
+RUN yum swap -y -- remove fakesystemd -- install systemd systemd-libs epel-release && yum clean all
+
+# Install FreeIPA client and other utils
+RUN yum install -y ipa-client dbus-python perl 'perl(Data::Dumper)' \
+    'perl(Time::HiRes)' openssh-clients openssh-server openldap-clients \
+    sudo less which jq screen vim xterm \
+    && yum clean all
 
 ADD dbus.service /etc/systemd/system/dbus.service
 RUN ln -sf dbus.service /etc/systemd/system/messagebus.service
@@ -15,6 +22,10 @@ RUN ln -sf /bin/true /bin/hostnamectl
 ADD systemctl /usr/bin/systemctl
 ADD ipa-client-configure-first /usr/sbin/ipa-client-configure-first
 
-RUN chmod -v +x /usr/bin/systemctl /usr/sbin/ipa-client-configure-first
+ADD scripts/dex-k8s.sh /usr/local/bin/
+ADD scripts/get_token.sh /usr/local/bin/
+ADD scripts/kubectl.sh /etc/profile.d/
+
+RUN chmod -v +x /usr/bin/systemctl /usr/sbin/ipa-client-configure-first /usr/local/bin/*
 
 ENTRYPOINT /usr/sbin/ipa-client-configure-first
---
+++ scripts/get_token.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+decode_base64_url() {
+  local len=$((${#1} % 4))
+  local result="$1"
+  if [ $len -eq 2 ]; then result="$1"'=='
+  elif [ $len -eq 3 ]; then result="$1"'=' 
+  fi
+  echo "$result" | tr '_-' '/+' | openssl enc -d -base64
+}
+
+decode_jwt(){
+   decode_base64_url $(echo -n $2 | cut -d "." -f $1) | jq .
+}
+
+is_expired(){
+  payload=$(decode_jwt 2 "$1")
+  expTime=$(echo $payload | jq .exp)
+  curTime=$(date +%s)
+  if [ "$curTime" -gt "$expTime" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "WARNING: This script should be sourced."
+fi
+
+KUBECTL=$(which --skip-alias kubectl)
+
+#export g_k8s_debug=1
+
+
+#TODO(mattymo): Use the following approach instead to get user email address:
+## SAB_K8S_UID=$(sudo bash -c "\
+##   kinit host/jumpbox.default.svc.cluster.local@CLUSTER.LOCAL -k -t /etc/krb5.keytab && \
+##   ipa user-show `whoami` --raw | grep mail | cut -d' ' -f4")
+
+if [ -z "$SAB_K8S_UID" ]; then
+  echo -n "Enter your email address: "
+  read SAB_K8S_UID
+fi
+
+if [ -z "$SAB_K8S_PWD" ]; then
+  echo -n "Enter password for $SAB_K8S_UID: "
+  read SAB_K8S_PWD
+fi
+
+DEFAULT_SAB_K8S_DEX_HOST=https://dex.kube-system.svc.cluster.local
+DEFAULT_SAB_K8S_DEX_WORKER_HOST=http://dex-worker.kube-system.svc.cluster.local
+if [ -z "$SAB_K8S_DEX_HOST" ]; then
+  if [ -n "$DOMAINNAME" ]; then
+    DEX_DOMAINNAME="kube-system.svc.$(echo $DOMAINNAME | cut -d'.' -f3-)"
+    SAB_K8S_DEX_HOST="https://dex.${DEX_DOMAINNAME}"
+    SAB_K8S_DEX_WORKER_HOST="https://dex-worker.${DEX_DOMAINNAME}"
+  else
+    echo -n "Enter Dex hostname (example: https://dex.kube-system.svc.cluster.local): "
+    read SAB_K8S_DEX_HOST
+    echo -n "Enter Dex Worker hostname (example: https://dex-worker.kube-system.svc.cluster.local): "
+    read SAB_K8S_DEX_WORKER_HOST
+    # Set to default if not specified
+    SAB_K8S_DEX_HOST=${SAB_K8S_DEX_HOST:-$DEFAULT_SAB_K8S_DEX_HOST}
+    SAB_K8S_DEX_WORKER_HOST=${SAB_K8S_DEX_WORKER_HOST:-$DEFAULT_SAB_K8S_DEX_WORKER_HOST}
+  fi
+fi
+
+export SAB_K8S_UID
+export SAB_K8S_PWD
+export SAB_K8S_DEX_HOST
+export SAB_K8S_DEX_WORKER_HOST
+
+# Only fetch token if it is not set or is expired
+if [ -z "$JWT" ] || is_expired "$JWT"; then
+  JWT=$(dex-k8s.sh dex-login)
+  if [ $? -ne 0 ]; then
+    echo "Login failed!"
+    return 1
+  fi
+  export JWT
+fi
+
+# Only write kubeconfig if JWT is missing
+if ! grep -q "$JWT" ~/.kube/config; then
+  $KUBECTL config set-credentials $SAB_K8S_UID --token="$JWT" > /dev/null
+  $KUBECTL config set-cluster default --server=https://kubernetes.default --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt > /dev/null
+  $KUBECTL config set-context default --cluster=default --user=$SAB_K8S_UID > /dev/null
+  $KUBECTL config use-context default > /dev/null
+fi
